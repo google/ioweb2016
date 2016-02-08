5ea651c01e47e6c622af4465df1fac943df46386
Jeff Posnick <jeffy@google.com>
2016-02-08 15:17:43 +0000

    userVisible + userVisibleOnly

--- app/scripts/helper/notifications.js
+++ app/scripts/helper/notifications.js
@@ -89,7 +89,11 @@ IOWA.Notifications = IOWA.Notifications || (function() {
     }
 
     return navigator.serviceWorker.ready.then(function(registration) {
-      return registration.pushManager.subscribe({userVisibleOnly: true});
+      return registration.pushManager.subscribe({
+        // Legacy support for userVisible; see https://github.com/w3c/push-api/issues/137
+        userVisible: true,
+        userVisibleOnly: true
+      });
     }).then(function(subscription) {
       if (subscription && subscription.endpoint) {
         // See https://groups.google.com/a/chromium.org/d/msg/blink-dev/CK13omVO5ds/fR6sdPxsaasJ
